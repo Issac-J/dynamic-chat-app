0a1c683f8acd92ceddd92b9170a4c54db7b94c45
EggsyJ <58285674+JeonChanMin@users.noreply.github.com>
2023-05-11 20:36:23 +0900

    Set up and Created express server

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+const server = require("http").createServer(app);
+const PORT = 8000;
+
+app.use(express.static(path.join(__dirname + "/public")));
+
+app.listen(PORT, () => {
+  console.log(`Server app listen on Port: ${PORT}`);
+});
